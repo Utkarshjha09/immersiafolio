4b14859f763466b1e95290204f475efbba29965f
Utkarsh Jha <Utkarshjha.999@gmail.com>
2025-06-29 13:01:13 +0000

    no my image name is utkarshjha.jpg

--- src/components/profile-card.tsx
+++ src/components/profile-card.tsx
@@ -34,7 +34,7 @@ const easeInOutCubic = (x: number) =>
   x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
 
 const ProfileCardComponent = ({
-  avatarUrl = "/utkarshjha.png",
+  avatarUrl = "/utkarshjha.jpg",
   iconUrl = "",
   grainUrl = "",
   behindGradient,
